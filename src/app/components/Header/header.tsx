eef799ce2fec7d537b03b544d49d877ca11b808e
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-17 23:07:03 -0300

    ui: agregar un h1 temporal para la pag

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,7 +1,11 @@
 import "./styles/header.css";
 
 const Header = () => {
-  return <header className="cabecera"></header>;
+  return (
+    <header className="cabecera">
+      <h1 className="cabecera__titulo_pag">Yaqui-Sieras</h1>
+    </header>
+  );
 };
 
 export default Header;
