84a0040e98b4d0513f358acfea54ade575c515b4
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-17 23:08:05 -0300

    Refactor: componetizar la presentación de la home

--- src/app/pages/Home/home.tsx
+++ src/app/pages/Home/home.tsx
@@ -1,13 +1,10 @@
 import "./styles/home.css";
+import Presentacion from "./components/Presentacion/presentacion.tsx";
 
 const Home = () => {
   return (
     <section className="home">
-      <article className="presentacion">
-        <h1 id="titulo" className="presentacion__titulo">
-          Bienvenidos a mi sitio web personal
-        </h1>
-      </article>
+      <Presentacion />
     </section>
   );
 };
