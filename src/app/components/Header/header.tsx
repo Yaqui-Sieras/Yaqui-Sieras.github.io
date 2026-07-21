c806c9d33ea965e5b385e0ecba5a04394f3bdeea
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-21 19:16:49 -0300

    Refactor: Cambiar h1 principal de texto a logo

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,9 +1,12 @@
 import "./styles/header.css";
+import logo from "./imgs/logo-GithubPage.png";
 
 const Header = () => {
   return (
     <header className="cabecera">
-      <h1 className="cabecera__titulo_pag">Yaqui-Sieras</h1>
+      <h1 className="cabecera__titulo_pag">
+        <img className="logo" alt="Logo" src={logo} />
+      </h1>
     </header>
   );
 };
