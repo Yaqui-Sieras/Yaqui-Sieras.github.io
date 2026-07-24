e34c2e50ca37674f44393fc8a2e89dc57f1c1f61
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-24 17:04:06 -0300

    Feat: Agregar un Hub central de enlaces

--- src/app/pages/Home/home.tsx
+++ src/app/pages/Home/home.tsx
@@ -1,10 +1,12 @@
 import "./styles/home.css";
 import Presentacion from "./components/Presentacion/presentacion.tsx";
+import MasEnlaces from "./components/MasEnlaces/mas-enlaces.tsx";
 
 const Home = () => {
   return (
     <section className="home">
       <Presentacion />
+      <MasEnlaces />
     </section>
   );
 };
