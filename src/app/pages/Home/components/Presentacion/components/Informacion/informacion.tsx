0de870021cca4a9b089e7afe9c34fbd47bde7a3e
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-24 17:02:46 -0300

    UI: Simplificar maquetado de la presentacion y cambiar estilos del componente

--- src/app/pages/Home/components/Presentacion/components/Informacion/informacion.tsx
+++ src/app/pages/Home/components/Presentacion/components/Informacion/informacion.tsx
@@ -3,20 +3,20 @@ import "./styles/informacion.css";
 const Informacion = () => {
   return (
     <div className="informacion">
-      <div className="informacion__cabecera">
-        <h1 className="cabecera__titulo">
-          Yaqueline <span className="cabecera__github">GH: Yaqui-Sieras</span>
-        </h1>
-        <h2 className="cabecera__subtitulo">Desarrolladora Full Stack</h2>
+      <h1 className="info__titulo">Yaqueline</h1>
+      <h2 className="info_subtitulo">Desarrolladora Full Stack</h2>
+
+      <div className="info__enlaces">
+        <a className="enlaces__items enlaces__github">GH: Yaqui-Sieras</a>
+        <a className="enlaces__items enlaces__linkedin">IN: Yaqui-Sieras</a>
       </div>
-      <div className="informacion__cuerpo">
-        <p className="cuerpo__descripcion">
-          Apasionada desarrolladora web con experiencia en la creación de
-          aplicaciones y sitios web modernos y funcionales. Me especializo en
-          tecnologías como React, TypeScript y CSS, y siempre estoy
-          perfeccionando mis habilidades.
-        </p>
-      </div>
+
+      <p className="info__descripcion">
+        Apasionada desarrolladora web con experiencia en la creación de
+        aplicaciones y sitios web modernos y funcionales. Me especializo en
+        tecnologías como React, TypeScript y CSS, y siempre estoy perfeccionando
+        mis habilidades.
+      </p>
     </div>
   );
 };
