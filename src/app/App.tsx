555edbdc958d52781cdac187f174ad54a31c16ad
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-28 16:56:15 -0300

    Refactor: Usar SenteiCSS de resetCSS

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,9 @@
-import { useExternalCss } from "./hooks/useExternalCss";
 import "./App.css";
 import Header from "./components/Header/header.tsx";
 import Footer from "./components/Footer/footer.tsx";
 import Home from "./pages/Home/home.tsx";
 
 function App() {
-  // Ejecutamos el hook pasándole las URLs
-  useExternalCss({
-    onlineUrl: "https://yaqui-sieras.github.io/miResetCSS/miReset.css",
-    localUrl: "/css/miReset.css",
-  });
-
   return (
     <>
       <Header />
